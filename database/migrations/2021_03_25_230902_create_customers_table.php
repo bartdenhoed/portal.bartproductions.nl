1e644e0c397a4abab93f3c80506634ba82551bfc
Bart den Hoed <git@bartdenhoed.nl>
2021-04-02 18:18:01 +0200

    Add customer

---
+++ database/migrations/2021_03_25_230902_create_customers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('client_number');
+            $table->integer('client_email');
+            $table->string('company_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
